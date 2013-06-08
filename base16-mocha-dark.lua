9a067c9055d77d8e90c78323cf67679486b61f2b
Robert Gieseke <robert.gieseke@gmail.com>
2013-06-08 18:58:52 +0200

    Add Base16 themes

---
+++ base16-mocha-dark.lua
@@ -0,0 +1,90 @@
+-- Mocha theme for Textadept
+-- Theme author: Chris Kempson (http://chriskempson.com)
+-- Build with Base16 (https://github.com/chriskempson/base16)
+-- Repository: https://github.com/rgieseke/ta-themes
+
+local buffer = buffer
+local property, property_int = buffer.property, buffer.property_int
+
+property['color.base00'] = 0x28323B
+property['color.base01'] = 0x364653
+property['color.base02'] = 0x405264
+property['color.base03'] = 0x5a707e
+property['color.base04'] = 0xadafb8
+property['color.base05'] = 0xc6c8d0
+property['color.base06'] = 0xdde1e9
+property['color.base07'] = 0xebeef5
+property['color.base08'] = 0x7760cb
+property['color.base09'] = 0x718bd2
+property['color.base0A'] = 0x87bcf4
+property['color.base0B'] = 0x5bb5be
+property['color.base0C'] = 0xa4bd7b
+property['color.base0D'] = 0xb5b38a
+property['color.base0E'] = 0xb99ba8
+property['color.base0F'] = 0x8495bb
+
+-- Default font.
+property['font'], property['fontsize'] = 'Bitstream Vera Sans Mono', 10
+if WIN32 then
+  property['font'] = 'Courier New'
+elseif OSX then
+  property['font'], property['fontsize'] = 'Monaco', 12
+end
+
+-- Token styles.
+property['style.nothing'] = ''
+property['style.class'] = 'fore:%(color.base0A)'
+property['style.comment'] = 'fore:%(color.base03)'
+property['style.constant'] = 'fore:%(color.base09)'
+property['style.error'] = 'fore:%(color.base08),italics'
+property['style.function'] = 'fore:%(color.base0E)'
+property['style.keyword'] = 'fore:%(color.base0E)'
+property['style.label'] = 'fore:%(color.base0A)'
+property['style.number'] = 'fore:%(color.base09)'
+property['style.operator'] = 'fore:%(color.base05)'
+property['style.regex'] = 'fore:%(color.base0C)'
+property['style.string'] = 'fore:%(color.base0B)'
+property['style.preprocessor'] = 'fore:%(color.base0A)'
+property['style.type'] = 'fore:%(color.base09)'
+property['style.variable'] = 'fore:%(color.base08)'
+property['style.whitespace'] = ''
+property['style.embedded'] = 'fore:%(color.base0F),back:%(color.base01)'
+property['style.identifier'] = '%(style.nothing)'
+
+-- Predefined styles.
+property['style.default'] = 'font:%(font),size:%(fontsize),'..
+                            'fore:%(color.base05),back:%(color.base00)'
+property['style.linenumber'] = 'fore:%(color.base02),back:%(color.base00)'
+property['style.bracelight'] = 'fore:%(color.base01),back:%(color.base0C)'
+property['style.bracebad'] = 'fore:%(color.base08)'
+property['style.controlchar'] = '%(style.nothing)'
+property['style.indentguide'] = 'fore:%(color.base03)'
+property['style.calltip'] = 'fore:%(color.base02),back:%(color.base07)'
+
+-- Multiple Selection and Virtual Space.
+--buffer.additional_sel_alpha =
+--buffer.additional_sel_fore =
+--buffer.additional_sel_back =
+--buffer.additional_caret_fore =
+
+-- Caret and Selection Styles.
+buffer:set_sel_fore(true, property_int['color.base00'])
+buffer:set_sel_back(true, property_int['color.base05'])
+--buffer.sel_alpha =
+buffer.caret_fore = property_int['color.base07']
+buffer.caret_line_back = property_int['color.base01']
+--buffer.caret_line_back_alpha =
+
+-- Fold Margin.
+buffer:set_fold_margin_colour(true, property_int['color.base00'])
+buffer:set_fold_margin_hi_colour(true, property_int['color.base05'])
+
+-- Fold Margin Markers.
+local c = _SCINTILLA.constants
+buffer.marker_fore[c.SC_MARKNUM_FOLDEROPEN] = property_int['color.base04']
+buffer.marker_back[c.SC_MARKNUM_FOLDEROPEN] = property_int['color.base04']
+buffer.marker_fore[c.SC_MARKNUM_FOLDER] = property_int['color.base04']
+buffer.marker_back[c.SC_MARKNUM_FOLDER] = property_int['color.base04']
+
+-- Long Lines.
+buffer.edge_colour = property_int['color.base01']
---
+++ base16-ocean-dark.lua
@@ -0,0 +1,90 @@
+-- Ocean theme for Textadept
+-- Theme author: Chris Kempson (http://chriskempson.com)
+-- Build with Base16 (https://github.com/chriskempson/base16)
+-- Repository: https://github.com/rgieseke/ta-themes
+
+local buffer = buffer
+local property, property_int = buffer.property, buffer.property_int
+
+property['color.base00'] = 0x3b302b
+property['color.base01'] = 0x463d34
+property['color.base02'] = 0x665b4f
+property['color.base03'] = 0x7e7365
+property['color.base04'] = 0xbaada7
+property['color.base05'] = 0xcec5c0
+property['color.base06'] = 0xe8e1df
+property['color.base07'] = 0xf5f1ef
+property['color.base08'] = 0x6a61bf
+property['color.base09'] = 0x7087d0
+property['color.base0A'] = 0x8bcbeb
+property['color.base0B'] = 0x8cbea3
+property['color.base0C'] = 0xb4b596
+property['color.base0D'] = 0xb3a18f
+property['color.base0E'] = 0xad8eb4
+property['color.base0F'] = 0x6779ab
+
+-- Default font.
+property['font'], property['fontsize'] = 'Bitstream Vera Sans Mono', 10
+if WIN32 then
+  property['font'] = 'Courier New'
+elseif OSX then
+  property['font'], property['fontsize'] = 'Monaco', 12
+end
+
+-- Token styles.
+property['style.nothing'] = ''
+property['style.class'] = 'fore:%(color.base0A)'
+property['style.comment'] = 'fore:%(color.base03)'
+property['style.constant'] = 'fore:%(color.base09)'
+property['style.error'] = 'fore:%(color.base08),italics'
+property['style.function'] = 'fore:%(color.base0E)'
+property['style.keyword'] = 'fore:%(color.base0E)'
+property['style.label'] = 'fore:%(color.base0A)'
+property['style.number'] = 'fore:%(color.base09)'
+property['style.operator'] = 'fore:%(color.base05)'
+property['style.regex'] = 'fore:%(color.base0C)'
+property['style.string'] = 'fore:%(color.base0B)'
+property['style.preprocessor'] = 'fore:%(color.base0A)'
+property['style.type'] = 'fore:%(color.base09)'
+property['style.variable'] = 'fore:%(color.base08)'
+property['style.whitespace'] = ''
+property['style.embedded'] = 'fore:%(color.base0F),back:%(color.base01)'
+property['style.identifier'] = '%(style.nothing)'
+
+-- Predefined styles.
+property['style.default'] = 'font:%(font),size:%(fontsize),'..
+                            'fore:%(color.base05),back:%(color.base00)'
+property['style.linenumber'] = 'fore:%(color.base02),back:%(color.base00)'
+property['style.bracelight'] = 'fore:%(color.base01),back:%(color.base0C)'
+property['style.bracebad'] = 'fore:%(color.base08)'
+property['style.controlchar'] = '%(style.nothing)'
+property['style.indentguide'] = 'fore:%(color.base03)'
+property['style.calltip'] = 'fore:%(color.base02),back:%(color.base07)'
+
+-- Multiple Selection and Virtual Space.
+--buffer.additional_sel_alpha =
+--buffer.additional_sel_fore =
+--buffer.additional_sel_back =
+--buffer.additional_caret_fore =
+
+-- Caret and Selection Styles.
+buffer:set_sel_fore(true, property_int['color.base00'])
+buffer:set_sel_back(true, property_int['color.base05'])
+--buffer.sel_alpha =
+buffer.caret_fore = property_int['color.base07']
+buffer.caret_line_back = property_int['color.base01']
+--buffer.caret_line_back_alpha =
+
+-- Fold Margin.
+buffer:set_fold_margin_colour(true, property_int['color.base00'])
+buffer:set_fold_margin_hi_colour(true, property_int['color.base05'])
+
+-- Fold Margin Markers.
+local c = _SCINTILLA.constants
+buffer.marker_fore[c.SC_MARKNUM_FOLDEROPEN] = property_int['color.base04']
+buffer.marker_back[c.SC_MARKNUM_FOLDEROPEN] = property_int['color.base04']
+buffer.marker_fore[c.SC_MARKNUM_FOLDER] = property_int['color.base04']
+buffer.marker_back[c.SC_MARKNUM_FOLDER] = property_int['color.base04']
+
+-- Long Lines.
+buffer.edge_colour = property_int['color.base01']
