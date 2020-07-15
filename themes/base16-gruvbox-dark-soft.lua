16d01e60d89c143416bb1bd1e43d58119ec8f61c
Robert Gieseke <rob.g@web.de>
2020-07-15 13:45:07 +0200

    Update themes

--- themes/base16-gruvbox-dark-soft.lua
+++ themes/base16-gruvbox-dark-soft.lua
@@ -5,62 +5,61 @@
 -- Template adapted from Textadept's default templates
 -- Copyright 2007-2020 Mitchell mitchell.att.foicica.com. MIT licensed.
 
-local view = view
-local property, property_int = view.property, view.property_int
+local view, colors, styles = view, lexer.colors, lexer.styles
 
 -- Base16 colors
-property['color.base00'] = 0x2f3032
-property['color.base01'] = 0x36383c
-property['color.base02'] = 0x454950
-property['color.base03'] = 0x545c66
-property['color.base04'] = 0x93aebd
-property['color.base05'] = 0xa1c4d5
-property['color.base06'] = 0xb2dbeb
-property['color.base07'] = 0xc7f1fb
-property['color.base08'] = 0x3449fb
-property['color.base09'] = 0x1980fe
-property['color.base0A'] = 0x2fbdfa
-property['color.base0B'] = 0x26bbb8
-property['color.base0C'] = 0x7cc08e
-property['color.base0D'] = 0x98a583
-property['color.base0E'] = 0x9b86d3
-property['color.base0F'] = 0x0e5dd6
+colors.base00 = 0x2f3032
+colors.base01 = 0x36383c
+colors.base02 = 0x454950
+colors.base03 = 0x545c66
+colors.base04 = 0x93aebd
+colors.base05 = 0xa1c4d5
+colors.base06 = 0xb2dbeb
+colors.base07 = 0xc7f1fb
+colors.base08 = 0x3449fb
+colors.base09 = 0x1980fe
+colors.base0A = 0x2fbdfa
+colors.base0B = 0x26bbb8
+colors.base0C = 0x7cc08e
+colors.base0D = 0x98a583
+colors.base0E = 0x9b86d3
+colors.base0F = 0x0e5dd6
 
 -- Default font.
-property['font'], property['fontsize'] = 'Bitstream Vera Sans Mono', 10
-if WIN32 then
-  property['font'] = 'Courier New'
-elseif OSX then
-  property['font'], property['fontsize'] = 'Monaco', 12
+if not font then
+  font = WIN32 and 'Courier New' or OSX and 'Monaco' or
+    'Bitstream Vera Sans Mono'
 end
+if not size then size = not OSX and 10 or 12 end
 
 -- Predefined styles.
-property['style.default'] = 'font:$(font),size:$(fontsize),'..
-                            'fore:$(color.base05),back:$(color.base00)'
-property['style.linenumber'] = 'fore:$(color.base04),back:$(color.base00)'
---property['style.controlchar'] =
-property['style.indentguide'] = 'fore:$(color.base03)'
-property['style.calltip'] = 'fore:$(color.base04),back:$(color.base01)'
-property['style.folddisplaytext'] = 'fore:$(color.base01)'
+styles.default = {
+  font = font, size = size, fore = colors.base05, back = colors.base00
+}
+styles.linenumber = {fore = colors.base04, back = colors.base00}
+--styles.control_char =
+styles.indent_guide = {fore = colors.base03}
+styles.call_tip = {fore = colors.base04, back = colors.base01}
+styles.fold_display_text = {fore = colors.base01}
 
 -- Token styles.
-property['style.class'] = 'fore:$(color.base0A)'
-property['style.comment'] = 'fore:$(color.base03)'
-property['style.constant'] = 'fore:$(color.base09)'
-property['style.embedded'] = 'fore:$(color.base0F),back:$(color.base01)'
-property['style.error'] = 'fore:$(color.base08),italics'
-property['style.function'] = 'fore:$(color.base0D)'
-property['style.identifier'] = ''
-property['style.keyword'] = 'fore:$(color.base0E)'
-property['style.label'] = 'fore:$(color.base08)'
-property['style.number'] = 'fore:$(color.base09)'
-property['style.operator'] = 'fore:$(color.base05)'
-property['style.preprocessor'] = 'fore:$(color.base0B)'
-property['style.regex'] = 'fore:$(color.base0C)'
-property['style.string'] = 'fore:$(color.base0B)'
-property['style.type'] = 'fore:$(color.base0A)'
-property['style.variable'] = 'fore:$(color.base08)'
-property['style.whitespace'] = ''
+styles.class = {fore = colors.base0A}
+styles.comment = {fore = colors.base03}
+styles.constant = {fore = colors.base09}
+styles.embedded = {fore = colors.base0F, back = colors.base01}
+styles.error = {fore = colors.base08, italics = true}
+styles['function'] = {fore = colors.base0D}
+styles.identifier = {}
+styles.keyword = {fore = colors.base0E}
+styles.label = {fore = colors.base08}
+styles.number = {fore = colors.base09}
+styles.operator = {fore = colors.base05}
+styles.preprocessor = {fore = colors.base0B}
+styles.regex = {fore = colors.base0C}
+styles.string = {fore = colors.base0B}
+styles.type = {fore = colors.base0A}
+styles.variable = {fore = colors.base08}
+styles.whitespace = {}
 
 -- Multiple Selection and Virtual Space
 --view.additional_sel_alpha =
@@ -69,49 +68,45 @@ property['style.whitespace'] = ''
 --view.additional_caret_fore =
 
 -- Caret and Selection Styles.
-view:set_sel_fore(true, property_int['color.base06'])
-view:set_sel_back(true, property_int['color.base02'])
+view:set_sel_fore(true, colors.base06)
+view:set_sel_back(true, colors.base02)
 --view.sel_alpha =
-view.caret_fore = property_int['color.base05']
-view.caret_line_back = property_int['color.base01']
+view.caret_fore = colors.base05
+view.caret_line_back = colors.base01
 --view.caret_line_back_alpha =
 
 -- Fold Margin.
-view:set_fold_margin_color(true, property_int['color.base00'])
-view:set_fold_margin_hi_color(true, property_int['color.base00'])
+view:set_fold_margin_color(true, colors.base00)
+view:set_fold_margin_hi_color(true, colors.base00)
 
 -- Markers.
-local MARK_BOOKMARK = textadept.bookmarks.MARK_BOOKMARK
---view.marker_fore[MARK_BOOKMARK] = property_int['color.base00']
-view.marker_back[MARK_BOOKMARK] = property_int['color.base0B']
---view.marker_fore[textadept.run.MARK_WARNING] = property_int['color.base00']
-view.marker_back[textadept.run.MARK_WARNING] = property_int['color.base0E']
---view.marker_fore[textadept.run.MARK_ERROR] = property_int['color.base00']
-view.marker_back[textadept.run.MARK_ERROR] = property_int['color.base08']
+--view.marker_fore[textadept.bookmarks.MARK_BOOKMARK] = colors.base00
+view.marker_back[textadept.bookmarks.MARK_BOOKMARK] = colors.base0B
+--view.marker_fore[textadept.run.MARK_WARNING] = colors.base00
+view.marker_back[textadept.run.MARK_WARNING] = colors.base0E
+--view.marker_fore[textadept.run.MARK_ERROR] = colors.base00
+view.marker_back[textadept.run.MARK_ERROR] = colors.base08
 for i = view.MARKNUM_FOLDEREND, view.MARKNUM_FOLDEROPEN do -- fold margin
-  view.marker_fore[i] = property_int['color.base00']
-  view.marker_back[i] = property_int['color.base03']
-  view.marker_back_selected[i] = property_int['color.base02']
+  view.marker_fore[i] = colors.base00
+  view.marker_back[i] = colors.base03
+  view.marker_back_selected[i] = colors.base02
 end
 
 -- Indicators.
-view.indic_fore[ui.find.INDIC_FIND] = property_int['color.base09']
+view.indic_fore[ui.find.INDIC_FIND] = colors.base09
 view.indic_alpha[ui.find.INDIC_FIND] = 255
-local INDIC_BRACEMATCH = textadept.editing.INDIC_BRACEMATCH
-view.indic_fore[INDIC_BRACEMATCH] = property_int['color.base06']
-local INDIC_HIGHLIGHT = textadept.editing.INDIC_HIGHLIGHT
-view.indic_fore[INDIC_HIGHLIGHT] = property_int['color.base07']
-view.indic_alpha[INDIC_HIGHLIGHT] = 255
-local INDIC_PLACEHOLDER = textadept.snippets.INDIC_PLACEHOLDER
-view.indic_fore[INDIC_PLACEHOLDER] = property_int['color.base04']
+view.indic_fore[textadept.editing.INDIC_BRACEMATCH] = colors.base06
+view.indic_fore[textadept.editing.INDIC_HIGHLIGHT] = colors.base07
+view.indic_alpha[textadept.editing.INDIC_HIGHLIGHT] = 255
+view.indic_fore[textadept.snippets.INDIC_PLACEHOLDER] = colors.base04
 
 -- Call tips.
-view.call_tip_fore_hlt = property_int['color.base06']
+view.call_tip_fore_hlt = colors.base06
 
 -- Long Lines.
-view.edge_color = property_int['color.base02']
+view.edge_color = colors.base02
 
 -- Add red, green, and yellow for diff lexer.
-property['color.red'] = property['color.base08']
-property['color.green'] = property['color.base0B']
-property['color.yellow'] = property['color.base0E']
+colors.red = colors.base08
+colors.green = colors.base0B
+colors.yellow = colors.base0E
